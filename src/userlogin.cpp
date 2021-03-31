0db50506509cc4f3a305648c52a9211d98768439
Arealis <nate.nick.will@gmail.com>
2021-03-31 19:03:29 -0400

    Change license
    The GNU license fits the needs of this program better than the MIT
    license.

--- src/userlogin.cpp
+++ src/userlogin.cpp
@@ -1,3 +1,23 @@
+/*
+    SIM - the Simple Inventory Manager
+    Copyright (C) 2021 Arealis
+
+    This file is part of Simple Inventory Manager.
+
+    Simple Inventory Manager is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Simple Inventory Manager is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Simple Inventory Manager.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "userlogin.h"
 #include "ui_userlogin.h"
 
